8ad2a84ebbf1ba29bf387dd42b2a60cfb0f29911
WuHarry <harrywhy32@gmail.com>
2016-09-10 20:39:33 +1000

    v0.01 Terrain without color

---
+++ Assets/Scripts/KeyboardController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardController : MonoBehaviour {
+
+    public float speed = 1.0f;
+    public float rotateSpeed = 0.5f;
+    public float mouseMovement;
+
+    void Start()
+    {
+        this.transform.localPosition = new Vector3(256f, 300f, 20f);
+        this.transform.localRotation *= Quaternion.AngleAxis(40f, Vector3.right);        
+    }
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKey(KeyCode.W)){
+            //keyboard w move forward the camera
+            this.transform.Translate(Vector3.forward * speed);
+        }
+        if(Input.GetKey(KeyCode.S)){
+            //keyboard s move backward the camera   
+            this.transform.Translate(Vector3.back * speed);
+        }
+        if(Input.GetKey(KeyCode.A)){
+            //keyboard a move left the camera
+            this.transform.Translate(Vector3.left * speed);
+        }
+        if(Input.GetKey(KeyCode.D)){
+            //keyboard a move right the camera
+            this.transform.Translate(Vector3.right * speed);
+        }
+        if(Input.GetKey(KeyCode.Q)){
+            //keyboard q roll the camera
+            this.transform.localRotation *= Quaternion.AngleAxis(rotateSpeed, Vector3.forward);
+        }
+        if(Input.GetKey(KeyCode.E)){
+            //keyboard e roll the camera
+            this.transform.localRotation *= Quaternion.AngleAxis(rotateSpeed, Vector3.back);
+        }
+        //Control the camera pitch and yaw by click and hold the mouse
+        //if (Input.GetKey(KeyCode.Mouse0))
+        //{
+        //    this.transform.localRotation *= Quaternion.AngleAxis(rspeed, Vector3.right);
+        //}
+        //if (Input.GetKey(KeyCode.Mouse1))
+        //{
+        //    this.transform.localRotation *= Quaternion.AngleAxis(rspeed, Vector3.left);
+        //}
+
+        //Control the camera pitch and yaw by moving the mouse
+        if (Input.mousePresent == true)
+        {
+            mouseMovement = Input.GetAxis("Mouse Y") * rotateSpeed * 3;
+            this.transform.Rotate(-mouseMovement, 0, 0);
+        }
+    }
+
+}
---
+++ Assets/Scripts/WaterGenerator.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterGenerator : MonoBehaviour {
+
+    public Shader shader;
+    public PointLight pointlight;
+    public Terrain currentearth;
+    
+
+    void Start()
+    {
+        gameObject.GetComponent<Renderer>().material.color = new Color(0, 0.6f, 0.8f, 0.5f);
+    }
+
+    // Update is called once per frame
+    //void update()
+    //{
+    //    int HeightMapResolation;
+    //    int basicheight;
+    //    basicheight = currentearth.terrainData.heightmapHeight;
+    //    HeightMapResolation = currentearth.terrainData.heightmapResolution;
+    //    float[,] height = new float[HeightMapResolation, HeightMapResolation];
+    //    height = currentearth.terrainData.GetHeights(0, 0, HeightMapResolation, HeightMapResolation);
+
+    //    //Calculate the average height of the terrain
+    //    double sum = 0;
+    //    double avg = 0;
+    //    for (int i = 0; i < HeightMapResolation; i++)
+    //    {
+    //        for (int j = 0; j < HeightMapResolation; j++)
+    //        {
+    //            sum += height[i, j];
+    //        }
+    //    }
+    //    avg = sum / (HeightMapResolation * HeightMapResolation);
+
+    //    //tramsform the water plane to average height;
+    //    this.transform.position = new Vector3(256, (float)avg * 250, 256);
+    //}
+}
