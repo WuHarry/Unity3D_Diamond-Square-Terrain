d11fd546249bbf37e0125558387286d2e71409c5
WuHarry <harrywhy32@gmail.com>
2016-09-10 20:21:18 +1000

    v0.01 without terraincolor

---
+++ Assets/Scripts/AxisSpin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AxisSpin : MonoBehaviour
+{
+
+    public float spinSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localRotation *= Quaternion.AngleAxis(Time.deltaTime * spinSpeed, Vector3.right);
+    }
+}
